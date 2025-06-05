fe985d75b34ddb0f02b9cc552d4e07612d5cf68c
EOM-TAE-KYOUN\qopxc <qopxc124@gmail.com>
2025-06-05 19:40:40 +0900

    first comit

---
+++ remind/src/main/java/com/tae/remind/RemindApplication.java
@@ -0,0 +1,13 @@
+package com.tae.remind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindApplication.class, args);
+	}
+
+}
---
+++ remind/src/test/java/com/tae/remind/RemindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tae.remind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
